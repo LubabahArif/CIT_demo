d6dce3b34bb88292361a9a623d312183be4a1dff
LubabahArif <arif547ah@gmail.com>
2024-07-13 01:59:15 -0700

    Add files via upload

---
+++ variablela.py
@@ -0,0 +1,23 @@
+#question no 1
+base=8
+height=10
+f=0.5
+area =f*height*base
+print("area of a triangle:",area)
+
+#question no 2
+side = 8
+side = 8
+area = side*side
+print("area of a square:", area)
+
+#question no 3
+radius=5
+pie=3.142
+area=radius*radius*pie
+print('area of circle:',area)
+
+
+
+
+
