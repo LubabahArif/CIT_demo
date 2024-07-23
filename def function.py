d6e7f769c2887abd1121ddaa6247e26ca79c7a46
LubabahArif <arif547ah@gmail.com>
2024-07-23 08:44:40 -0700

    Add files via upload

---
+++ def function.py
@@ -0,0 +1,12 @@
+def max_of_three(a,b,c):
+    if a >= b and a >= c:
+        print(a)
+    elif b>= a and b>= c:
+        print(b)
+    else:
+        print(c)
+
+max_of_three(10,10,3)
+max_of_three(45,55,65)
+max_of_three(9,10,3)
+
